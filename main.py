e7fccef24515ead95f94a8fe514b63e90e428206
mujtabamushtaq <48491520+mujtabamushtaq@users.noreply.github.com>
2022-08-10 00:53:54 +0500

    Changes to sbs scrapper

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 import time
 import requests
 import hashlib
+from urllib.parse import unquote
 
 from PIL import Image
 naver_css=[
@@ -15,7 +16,6 @@ def fetch_image_urls(wd: webdriver,site: str, q: str, m: str = '', sleep_between
     def scroll_to_end(wd):
         wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-        time.sleep(sleep_between_interactions)
 
         # build the google query
     if site == 'sbs':
@@ -24,10 +24,14 @@ def fetch_image_urls(wd: webdriver,site: str, q: str, m: str = '', sleep_between
     elif site == 'naver':
         search_url = f"https://m.post.naver.com/viewer/postView.naver?volumeNo={q}&memberNo={m}"
         css_class = naver_css
+    elif site == 'gfycat':
+        search_url = q
+        css_class = 'image media'
+
 
     # load the page
     wd.get(search_url)
-
+    time.sleep(sleep_between_interactions)
     image_urls = set()
 
     scroll_to_end(wd)
@@ -41,9 +45,7 @@ def fetch_image_urls(wd: webdriver,site: str, q: str, m: str = '', sleep_between
     return image_urls
 
 
-
-
-def persist_image(folder_path:str,url:str):
+def persist_image(folder_path: str, url: str, q: str, type: str):
     try:
         image_content = requests.get(url).content
 
@@ -53,7 +55,8 @@ def persist_image(folder_path:str,url:str):
     try:
         image_file = io.BytesIO(image_content)
         image = Image.open(image_file).convert('RGB')
-        file_path = os.path.join(folder_path,hashlib.sha1(image_content).hexdigest()[:10] + '.jpg')
+        url = unquote(url)
+        file_path = os.path.join(folder_path, q + '_' + url.split('/')[-1][:-4] + type)
         with open(file_path, 'wb') as f:
             image.save(f, "JPEG", quality=100)
         print(f"SUCCESS - saved {url} - as {file_path}")
@@ -61,17 +64,24 @@ def persist_image(folder_path:str,url:str):
         print(f"ERROR - Could not save {url} - {e}")
 
 
-def search_and_download(site: str, q: str, m: str, driver_path: str, target_path='E:/images', number_images=5):
+def search_and_download(site: str, q: str, m: str, driver_path: str, target_path='E:/images', number_images=5,
+                        type: str = '.jpg'):
     target_folder = target_path
 
     if not os.path.exists(target_folder):
         os.makedirs(target_folder)
 
     with webdriver.Chrome(executable_path=driver_path) as wd:
-        res = fetch_image_urls(wd=wd,site=site, q=q, m=m, sleep_between_interactions=10)
+        res = fetch_image_urls(wd=wd, site=site, q=q, m=m, sleep_between_interactions=20)
 
     for elem in res:
-        persist_image(target_folder, elem)
+        persist_image(target_folder, elem, q, type)
 
+site = 'sbs'
 
-search_and_download('naver', '32625398', '25831870', 'chromedriver.exe', number_images=100)
+if site == 'sbs':
+    for n in range(439773, 439779):
+        if n not in [439737]:
+            search_and_download(site, str(n), '54795', 'chromedriver.exe', number_images=100)
+else:
+    search_and_download(site, '34098471', '16220685', 'chromedriver.exe', number_images=100)
